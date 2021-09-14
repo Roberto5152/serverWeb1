55dee369cb27c561ee35fcdac0c2185720b77aa5
Roberto5152 <89862750+Roberto5152@users.noreply.github.com>
2021-09-14 11:16:08 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,32 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
-	//alert("led on");
-	console.log("led on");
-	//document.getElementById("sensor").innerHTML="led on";
-	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
-    	client.send(message);
-  
-}
-function LED1_Off(){	
-	//alert("led off");
-	console.log("led off");
-	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
-    	client.send(message);
-	//document.getElementById("sensor").innerHTML="led off";
+function nuevo() {
+  console.log("Mensaje enviado");
+  texto = document.getElementById('enviarmensaje').value;
+  message = new Paho.MQTT.Message(texto);
+  message.destinationName = "carloscalderon_@hotmail.es/t2";
+  client.send(message);
 }
 
+function LED1_On() {
+  //alert("led on");
+  console.log("led on");
+  document.getElementById('estado').innerHTML='Prendido';
+  document.getElementById('imagen').src='https://indiehoy.com/wp-content/uploads/2018/03/stewie-padre-de-familia.jpg'
+  message = new Paho.MQTT.Message("ON");
+    message.destinationName = "carloscalderon_@hotmail.es/t2";
+    client.send(message);
+  
+}
+function LED1_Off(){
+  //alert("led off");
+  console.log("led off");
+  document.getElementById('estado').innerHTML='Apagado';
+  document.getElementById('imagen').src='https://www.lavanguardia.com/files/image_449_220/uploads/2018/03/19/5fa436c7233b0.jpeg'
+  message = new Paho.MQTT.Message("OFF");
+    message.destinationName = "carloscalderon_@hotmail.es/t2";
+    client.send(message);
+}
 
 
 
@@ -26,37 +35,35 @@ function LED1_Off(){
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
-  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
   // set callback handlers
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
+  client.onConnectionLost = onConnectionLost; //perdio conexion
+  client.onMessageArrived = onMessageArrived; //llego mensaje
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "carloscalderon_@hotmail.es",
+    password: "roberto51",
     onSuccess:onConnect,
     onFailure:doFail
   }
 
   // connect the client
   client.connect(options);
-   
+
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    client.subscribe("carloscalderon_@hotmail.es/t1");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
-    client.send(message);
-	
+    message.destinationName = "carloscalderon_@hotmail.es/t2";//añade el topico
+    client.send(message);// envia el mensaje
+  
   }
 
   function doFail(e){
     console.log(e);
-	
   }
 
   // called when the client loses its connection
@@ -68,7 +75,16 @@ function LED1_Off(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML=message.payloadString;
+    console.log("onMessageArrived:"+message.payloadString);// concatena el mensaje recibido
+	  document.getElementById("recibido").innerHTML=message.payloadString;//publica el mensaje al id asignado
+
+    if (message.payloadString == ("ON")){
+      document.getElementById('estado').innerHTML='Prendido';
+      document.getElementById('imagen').src='https://indiehoy.com/wp-content/uploads/2018/03/stewie-padre-de-familia.jpg'
+    }
+    if (message.payloadString == "OFF"){
+      document.getElementById('estado').innerHTML='Apagado';
+      document.getElementById('imagen').src='https://www.lavanguardia.com/files/image_449_220/uploads/2018/03/19/5fa436c7233b0.jpeg'
+    }
   }
   
---
+++ static/js/prueba.js
@@ -0,0 +1,20 @@
+
+//alert('Hola mundo');
+document.getElementById('sensor1').innerHTML='valor 1';
+
+function inicio() {
+  document.getElementById('sensor2').innerHTML='valor 2';
+
+}
+/*
+function LED1_On() {
+  console.log("led on");
+  document.getElementById('estado').innerHTML='Prendido';
+  document.getElementById('imagen').src='https://indiehoy.com/wp-content/uploads/2018/03/stewie-padre-de-familia.jpg'
+}
+function LED1_Off() {
+  console.log("led off");
+  document.getElementById('estado').innerHTML='Apagado';
+  document.getElementById('imagen').src='https://www.lavanguardia.com/files/image_449_220/uploads/2018/03/19/5fa436c7233b0.jpeg'
+}
+*/
